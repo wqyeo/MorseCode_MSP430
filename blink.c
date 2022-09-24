7019e0a678e1efed1926bd11851e51d28107a63b
Yeo Wen Qin, Kaynn <KaynnCahya@outlook.com>
2022-09-24 23:43:07 +0800

    Added functionality to toggle blink between desired port

--- blink.c
+++ blink.c
@@ -4,16 +4,33 @@
 /**
  * blink.c
  */
+void delay(){
+    volatile unsigned int j;
+    for(j=10000; j>0; --j);
+}
+
+void toggleblink(int portNum){
+    if (portNum == 1){
+        P1DIR |= 0x01;
+        P1OUT ^= 0x01;
+    } else if (portNum == 4){
+        P4DIR |= 0x80;
+        P4OUT ^= 0x80;
+    }
+}
+
 void main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;		// stop watchdog timer
-	P1DIR |= 0x01;					// configure P1.0 as output
 
 	volatile unsigned int i;		// volatile to prevent optimization
+	char message[] = "Hello World";
 
 	while(1)
 	{
-		P1OUT ^= 0x01;				// toggle P1.0
-		for(i=10000; i>0; i--);     // delay
+	    for (i = 0; message[i] != 0; ++i) {
+	        toggleblink(4);
+	        delay();
+	    }
 	}
 }

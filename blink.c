b88ffcc1c04cc7c86d8602f19a6009d597ed2864
Yeo Wen Qin, Kaynn <KaynnCahya@outlook.com>
2022-09-25 00:35:14 +0800

    Implemented Morse Code Blinking

--- blink.c
+++ blink.c
@@ -1,15 +1,52 @@
-#include <msp430.h>				
+#include <msp430.h>
 
+char morse_ref[36][5] = {
+                      // 0-9
+                      "33333",
+                      "13333",
+                      "11333",
+                      "11133",
+                      "11113",
+                      "11111",
+                      "31111",
+                      "33111",
+                      "33311",
+                      "33331",
+                      // Alphabets
+                      "13",
+                      "3111",
+                      "3131",
+                      "311",
+                      "1",
+                      "1131",
+                      "331",
+                      "1111",
+                      "11",
+                      "1333",
+                      "313",
+                      "1311",
+                      "33",
+                      "31",
+                      "333",
+                      "1331",
+                      "3313",
+                      "131",
+                      "111",
+                      "3",
+                      "113",
+                      "1113",
+                      "133",
+                      "3113",
+                      "3133",
+                      "3311"
+};
 
-/**
- * blink.c
- */
-void delay(){
+void delay() {
     volatile unsigned int j;
-    for(j=10000; j>0; --j);
+    for(j=12000; j>0; --j);
 }
 
-void toggleblink(int portNum){
+void toggle_blink(int portNum) {
     if (portNum == 1){
         P1DIR |= 0x01;
         P1OUT ^= 0x01;
@@ -19,18 +56,73 @@ void toggleblink(int portNum){
     }
 }
 
-void main(void)
-{
-	WDTCTL = WDTPW | WDTHOLD;		// stop watchdog timer
+void blink_morse(char blinkMessage[]) {
+    volatile unsigned int x;
+    for (x = 0; blinkMessage[x] != 0; ++x) {
+        int blinkDuration = blinkMessage[x] - '0';
 
-	volatile unsigned int i;		// volatile to prevent optimization
-	char message[] = "Hello World";
+        // Turn on and blink for 3 loops if long
+        // 1 loop if short.
+        toggle_blink(1);
+        while (blinkDuration > 0){
+            delay();
+            --blinkDuration;
+        }
+        // Turn off and delay before the next signal
+        toggle_blink(1);
+        delay();
+    }
+}
 
-	while(1)
-	{
+void main(void) {
+	WDTCTL = WDTPW | WDTHOLD;
+
+    char message[] = "HELLO WORLD";
+
+    volatile unsigned int i;
+    /* Un-comment this chunk if your message is in lower-case.
+     * (Though this just converts it to upper-case, so it is better
+     * if you just convert it yourself manually).
+     *
+    // Convert the message to all upper-case
+    for (i = 0; message[i]!='\0'; ++i) {
+       if(message[i] >= 'a' && message[i] <= 'z') {
+           message[i] = message[i] -32;
+       }
+    }
+    */
+
+	while(1) {
 	    for (i = 0; message[i] != 0; ++i) {
-	        toggleblink(4);
+	        int ascii = message[i];
+	        int offset = 0;
+	        if (ascii <= '9'){
+	            // current char is 0-9.
+	            offset = -('0');
+	        } else {
+	            // assume char is A-Z
+	            offset = -('A') + 11;
+	        }
+
+	        int index = ascii + offset;
+	        // If index out of range
+	        if (index < 0 || index >= 36){
+	            // Assume its a whitespace and just delay
+	            delay();
+	        } else {
+	            blink_morse(morse_ref[index]);
+	        }
+
+	        // 2 loop delay before the next
+	        delay();
 	        delay();
 	    }
+
+	    // To signal end-start of next loop.
+	    toggle_blink(4);
+        delay();
+        delay();
+        toggle_blink(4);
+        delay();
 	}
 }
